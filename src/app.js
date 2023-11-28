73ddaa689e9260933a06313cd0cf27d0e6f2ee2e
Benoit-Hayet <hayetbenoit@gmail.com>
2023-11-28 11:35:16 +0100

    Express 02

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 const express = require("express");
-
 const app = express();
+app.use(express.json());
 
 const movieControllers = require("./controllers/movieControllers");
 const userControllers = require("./controllers/userControllers");
 
 app.get("/api/movies", movieControllers.getMovies);
+app.post("/api/movies", movieControllers.postMovie);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.get("/api/users", userControllers.getUsers);
+app.post("/api/users", userControllers.postUsers);
 app.get("/api/users/:id", userControllers.getUsersById);
 
 module.exports = app;
--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -27,6 +27,23 @@ const movies = [
 
 const database = require("../../database");
 
+const postMovie = (req, res) => {
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query(
+      "INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
+      [title, director, year, color, duration]
+    )
+    .then(([result]) => {
+      res.status(201).send({ id: result.insertId });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
 const getMovies = (req, res) => {
   database
     .query("select * from movies")
@@ -59,4 +76,5 @@ const getMovieById = (req, res) => {
 module.exports = {
   getMovies,
   getMovieById,
+  postMovie,
 }
